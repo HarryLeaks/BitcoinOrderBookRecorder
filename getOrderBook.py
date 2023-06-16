52c7c9b40605c7342aa7334386b3e82038fbcba0
Harry Leaks <franciscomendesantos@gmail.com>
2023-06-16 16:10:11 +0100

    Add files via upload

--- getOrderBook.py
+++ getOrderBook.py
@@ -1,17 +1,17 @@
 import ccxt
 import csv
+import datetime
 
 # Create a Binance exchange object
 exchange = ccxt.binance()
 
 # Set the symbol and timeframe for the order book
 symbol = 'BTC/USDT'
-timeframe = '1m'
 
 # Open a CSV file for writing
 csv_file = open("order_book.csv", "w", newline="")
 csv_writer = csv.writer(csv_file)
-csv_writer.writerow(["Timestamp", "Symbol", "Price", "Quantity", "Side"])
+csv_writer.writerow(["Date", "Symbol", "Price", "Quantity", "Side"])
 
 try:
     while True:
@@ -20,12 +20,13 @@ try:
 
         # Process and save the order book data to CSV
         timestamp = exchange.milliseconds()
+        date = datetime.datetime.fromtimestamp(timestamp / 1000).strftime('%Y-%m-%d %H:%M:%S')
         for bid in order_book['bids']:
             price, quantity = bid
-            csv_writer.writerow([timestamp, symbol, price, quantity, "Bid"])
+            csv_writer.writerow([date, symbol, price, quantity, "Bid"])
         for ask in order_book['asks']:
             price, quantity = ask
-            csv_writer.writerow([timestamp, symbol, price, quantity, "Ask"])
+            csv_writer.writerow([date, symbol, price, quantity, "Ask"])
 
         csv_file.flush()
 
